db2d8488476e392a182ee2d70e35c440bef4d41f
d4md1n <d4md1n@hotmail.com>
2016-02-14 19:02:40 +0200

    fixaki

---
+++ Ebla/Controllers/AppController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Web.Http;
+
+namespace Ebla.Controllers
+{
+    public class AppController : ApiController
+    {
+        AppDomainController appDomain;
+
+        protected void init()
+        {
+            appDomain = new AppDomainController();
+        }
+
+
+        [HttpPost]
+        public string AddBookToUser([FromBody]JObject userBook)
+        {
+            init();
+            return appDomain.AddBookToUser(userBook);
+        }
+
+        [HttpPost]
+        public string RemoveBookFromUser([FromBody]JObject userBook)
+        {
+            init();
+            return appDomain.RemoveBookFromUser(userBook);
+        }
+
+
+    }
+}
